dd431482790fceb5d873d0be37ab9a4f2f5edf17
ZeelVanani <115966732+ZeelVanani@users.noreply.github.com>
2022-10-17 10:06:24 +0530

    Add files via upload

---
+++ Prac1c.java
@@ -0,0 +1,11 @@
+public class Prac1c 
+{
+    public static void main(String[] args) 
+    {
+        boolean b=false;
+        if (b==false) {
+            System.out.println("False");
+        }
+        else System.out.println("True");
+    }
+}
